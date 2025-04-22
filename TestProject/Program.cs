1345fe7e7a706cad8900f2a68bac5d585089656b
wroger1224 <wroger1224@gmail.com>
2025-04-22 15:03:46 -0400

    END Exercise - Explore data type casting and conversion

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -49,4 +49,61 @@
     Console.WriteLine($"ref_B[0]: {ref_B[0]}"); */
 }
 
-//Exercise - Explore data type casting and conversion
+//Convert data types using casting and conversion techniques in C# 
+{
+    //Exercise - Explore data type casting and conversion
+    {
+        //Write code that attempts to add an int and a string and save the result in an int
+        /* int first = 2;
+        string second = "4";
+        string result = first + second;
+        Console.WriteLine(result); */
+
+        //Is it possible that attempting to change the value's data type would result in a loss of information?
+        /* int myInt = 3;
+        Console.WriteLine($"int: {myInt}");
+
+        decimal myDecimal = myInt;
+        Console.WriteLine($"decimal: {myDecimal}"); */
+
+        //Perform a cast
+        /*  decimal myDecimal = 3.14m;
+         Console.WriteLine($"decimal: {myDecimal}");
+         // To perform a cast, you use the casting operator () to surround a data type, then place it next to the variable you want to convert (example: (int)myDecimal). You perform an explicit conversion to the defined cast data type (int).
+         int myInt = (int)myDecimal;
+         Console.WriteLine($"int: {myInt}"); */
+
+        //Determine if your conversion is a "widening conversion" or a "narrowing conversion"
+        /* decimal myDecimal = 1.23456789m;
+        float myFloat = (float)myDecimal;
+
+        Console.WriteLine($"Decimal: {myDecimal}");
+        Console.WriteLine($"Float: {myFloat}"); */
+
+        //Use ToString() to convert a number to a string
+        /* int first = 5;
+        int second = 7;
+        string message = first.ToString() + second.ToString();
+        Console.WriteLine(message); */
+
+        //Convert a string to an int using the Parse() helper method
+        /* string first = "5";
+        string second = "7";
+        int sum = int.Parse(first) + int.Parse(second);
+        Console.WriteLine(sum); */
+
+        //Convert a string to a int using the Convert class
+        /* string value1 = "5";
+        string value2 = "7";
+        int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+        Console.WriteLine(result); */
+
+        //Compare casting and converting a decimal into an int
+        /* int value = (int)1.5m; //casting truncates
+        Console.WriteLine(value);
+
+        int value2 = Convert.ToInt32(1.5m); // converting rounds up
+        Console.WriteLine(value2); */
+    }
+
+}
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d38ddeac34594b5f81444dcd65d699707a400267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2871392b135cf5a94ba1c51d09bf80bef19ef059")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
