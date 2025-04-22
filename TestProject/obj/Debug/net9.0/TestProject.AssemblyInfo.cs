98d54109256562362cbdabae2270ef8867646fca
wroger1224 <wroger1224@gmail.com>
2025-04-22 12:14:14 -0400

    END Exercise- Discover floating-point types

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6868c9e7323578afa23d1c371a86b69a10f41d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f6a718adf996b011ff921523f5ac72a6539834")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
