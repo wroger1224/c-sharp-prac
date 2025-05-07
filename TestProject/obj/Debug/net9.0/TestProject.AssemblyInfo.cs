fedad92b1570b86b6ef035d66eb9f161a70710bc
wroger1224 <wroger1224@gmail.com>
2025-05-07 12:14:43 -0400

    Did some codewars training - need to make new folder for that

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8d7b42388f643e6429ba25fff248ab364506a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ee2691aefff2c4df870712ed34fbbce9ef5b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
