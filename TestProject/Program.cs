9379e0d9f128e3da420739c0263eb80d3024ee25
wroger1224 <wroger1224@gmail.com>
2025-05-27 11:55:00 -0400

    Committing cause its been a minute

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -171,74 +171,112 @@
 
 //Perform operations on arrays using helper methods in C#
 {
+    //Exercise - Discover Sort() and Reverse()
+    {
+        /* string[] pallets = ["B14", "A11", "B12", "A13"];
+
+        Console.WriteLine("Sorted...");
+        Array.Sort(pallets);
+        foreach (var pallet in pallets)
+        {
+            Console.WriteLine($"-- {pallet}");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Reversed...");
+        Array.Reverse(pallets);
+        foreach (var pallet in pallets)
+        {
+            Console.WriteLine($"-- {pallet}");
+        } */
+    }
+
+    //Exercise - Explore Clear() and Resize()
+    {
+        /* string[] pallets = ["B14", "A11", "B12", "A13"];
+        Console.WriteLine("");
+
+        /* Console.WriteLine($"Before: {pallets[0].ToLower()}"); */
+        /* Array.Clear(pallets, 0, 2);
+        Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+        foreach (var pallet in pallets) Console.WriteLine($"-- {pallet}"); */
+        /* if (pallets[0] != null)
+        {
+            Console.WriteLine($"After: {pallets[0].ToLower()}");
+        } */
+
+        /* Console.WriteLine("");
+        Array.Resize(ref pallets, 6);
+        Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+        pallets[4] = "C01";
+        pallets[5] = "C02";
+
+        foreach (var pallet in pallets) Console.WriteLine($"-- {pallet}");
+
+        Console.WriteLine("");
+        Array.Resize(ref pallets, 3);
+        Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+        foreach (var pallet in pallets) Console.WriteLine($"-- {pallet}");  */
+    }
+
+    //Exercise - Discover Split() and Join()
+    {
+        /* string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+        //string result = new string(valueArray);
+        string result = string.Join(",", valueArray);
+        Console.WriteLine(result);
+
+        string[] items = result.Split(',');
+        foreach (string item in items)
+        {
+            Console.WriteLine(item);
+        } */
+    }
+
+    //Exercise - Complete a challenge to reverse words in a sentence
+    {
+        /* string pangram = "The quick brown fox jumps over the lazy dog";
+
+        char[] pangramArray = pangram.ToCharArray();
+        Array.Reverse(pangramArray);
+        string partOne = string.Join("", pangramArray);
+
+        string[] words = partOne.Split(' ');
+        Array.Reverse(words);
+
+        string final = string.Join(" ", words);
+        Console.WriteLine(final); */
+
+        // result: ehT kciuq nworb xof spmuj revo eht yzal god
+        //Their answer:
+        /*  string pangram = "The quick brown fox jumps over the lazy dog";
+
+        // Step 1
+        string[] message = pangram.Split(' ');
+
+        //Step 2
+        string[] newMessage = new string[message.Length];
+
+        // Step 3
+        for (int i = 0; i < message.Length; i++)
+        {
+            char[] letters = message[i].ToCharArray();
+            Array.Reverse(letters);
+            newMessage[i] = new string(letters);
+        }
+
+        //Step 4
+        string result = String.Join(" ", newMessage);
+        Console.WriteLine(result); */
+    }
 
 
 }
 
-//CodeWars Training
-{
-    //Implement a function which convert the given boolean value into its string representation.
-    //Note: Only valid inputs will be given
 
-    /*   public class kata
-  {
-      public static string BooleanToString(bool b)
-      {
-          if (b == true)
-          {
-              return "True";
-          }
-          else
-          {
-              return "False";
-          } */
-    // => b ? "True" : "False";  <<-- This is a more concise way to write the same logic using a ternary operator. Put it on the same line as the return statement.
-    //OR
-    //return b.ToString();  <<-- This is a more concise way to write the same logic using the built-in ToString() method of the bool type.
-    //}
-}
-
-{
-    //Calculate BMI
-    /* Write function bmi that calculates body mass index (bmi = weight / height2).
-
-if bmi <= 18.5 return "Underweight"
-
-if bmi <= 25.0 return "Normal"
-
-if bmi <= 30.0 return "Overweight"
-
-if bmi > 30 return "Obese" */
-    /*    Console.WriteLine("Enter your weight in pounds.");
-       double weight = Convert.ToDouble(Console.ReadLine());
-
-       Console.WriteLine("Enter your height in inches.");
-       double height = Convert.ToDouble(Console.ReadLine());
-
-       double bmi = (((weight * 703) / height) / height);
-       bmi = double.Round(bmi, 2, MidpointRounding.AwayFromZero);
-       Console.WriteLine($"Your calculated bmi is: {bmi}");
-
-       switch (bmi)
-       {
-           case <= 18.5:
-               Console.WriteLine("Underweight");
-               break;
-
-           case <= 25.0:
-               Console.WriteLine("Normal");
-               break;
-           case <= 30:
-               Console.WriteLine("Overweight");
-               break;
-
-           case > 30:
-               Console.WriteLine("Obese");
-               break;
-           default:
-               Console.WriteLine("Please check your numbers and try again");
-               break;
-       } */
-}
 
 
