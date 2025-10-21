661c8a1e587081eb831fcfa3acc69f2360078e0c
wroger1224 <wroger1224@gmail.com>
2025-10-21 14:57:57 -0400

    Finished the Module

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Choose the correct data type
+using System.Windows.Markup;
+
 {
     //Discover integral types
     /* Console.WriteLine("Signed integral types:");
@@ -665,7 +667,90 @@
 
         // Exercise - Build code with methods
         {
+            /*
+            if ipAddress consists of 4 numbers
+            and
+            if each ipAddress number has no leading zeroes
+            and
+            if each ipAddress number is in range 0 - 255
 
+            then ipAddress is valid
+
+            else ipAddress is invalid
+            */
+            /*  string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
+             string[] address;
+             bool validLength, validZeroes, validRange = false;
+
+             foreach (string ip in ipv4Input)
+             {
+                 address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+                 ValidateLength();
+                 ValidateZeroes();
+                 ValidateRange();
+
+                 if (validLength && validZeroes && validRange)
+                 {
+                     Console.WriteLine($"{ip} is a valid IPv4 address");
+                 }
+                 else Console.WriteLine($"{ip} is an invalid IPv4 address");
+             }
+
+             void ValidateLength()
+             {
+                 validLength = address.Length == 4;
+             }
+
+             void ValidateZeroes()
+             {
+                 foreach (string number in address)
+                 {
+                     if (number.Length > 1 && number.StartsWith("0"))
+                     {
+                         validZeroes = false;
+                         return;
+                     }
+                 }
+                 validZeroes = true;
+             }
+
+             void ValidateRange()
+             {
+                 foreach (string number in address)
+                 {
+                     int value = int.Parse(number);
+                     if (value < 0 || value > 255)
+                     {
+                         validRange = false;
+                         return;
+                     }
+                 }
+                 validRange = true;
+             }*/
+        }
+
+        // Exercise - Complete the challenge to create a reusable method
+        {
+            /*  FortuneTeller();
+
+             void FortuneTeller()
+             {
+                 Random random = new Random();
+                 int luck = random.Next(100);
+
+                 string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+                 string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+                 string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+                 string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+
+                 Console.WriteLine("A fortune teller whispers the following words:");
+                 string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+                 for (int i = 0; i < 4; i++)
+                 {
+                     Console.Write($"{text[i]} {fortune[i]} ");
+                 }
+             } */
         }
     }
 }
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a762c5869c0d12905b906658c5b9ae120f4082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfdbe068e243facfd6f1ab95e818612f31bb3434")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
