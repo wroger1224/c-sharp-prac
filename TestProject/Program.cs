cfdbe068e243facfd6f1ab95e818612f31bb3434
wroger1224 <wroger1224@gmail.com>
2025-10-21 12:29:00 -0400

    Starting the Create Methods module

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -571,10 +571,104 @@
          Console.WriteLine(quantity);
          Console.WriteLine(output); */
     }
-
-
 }
 
+//Create Methods in C# Console Applications
+{
+    //Write your first C# method
+    {
+        //Exercise - Create your first method
+        {
+            /* Console.WriteLine("Generating random numbers:");
+            DisplayRandomNumbers();
+
+            void DisplayRandomNumbers()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write($"{random.Next(1, 100)} ");
+                }
+                Console.WriteLine();
+            } */
+        }
+
+        //Exercise - Create reusable methods
+        {
+
+            /*  int[] times = { 800, 1200, 1600, 2000 };
+             int diff = 0;
+
+             Console.WriteLine("Enter current GMT");
+             int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Current Medicine Schedule:");
+             DisplayTimes();
+
+             Console.WriteLine("Enter new GMT");
+             int newGMT = Convert.ToInt32(Console.ReadLine());
+
+             if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+             {
+                 Console.WriteLine("Invalid GMT");
+             }
+             else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+             {
+                 diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+                 AdjustTimes();
+             }
+             else
+             {
+                 diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+                 AdjustTimes();
+             }
+
+             Console.WriteLine("New Medicine Schedule:");
+
+             DisplayTimes();
+
+             void DisplayTimes()
+             {
+                 foreach (int val in times)
+                 {
+                     string time = val.ToString();
+                     int len = time.Length;
+
+                     if (len >= 3)
+                     {
+                         time = time.Insert(len - 2, ":");
+                     }
+                     else if (len == 2)
+                     {
+                         time = time.Insert(0, "0:");
+                     }
+                     else
+                     {
+                         time = time.Insert(0, "0:0");
+                     }
+
+                     Console.Write($"{time} ");
+                 }
+                 Console.WriteLine();
+             }
+
+             void AdjustTimes()
+             {
+                 /* Adjust the times by adding the difference, keeping the value within 24 hours */
+            /*for (int i = 0; i < times.Length; i++)
+            {
+                times[i] = ((times[i] + diff)) % 2400;
+            }
+        } */
+        }
+
+        // Exercise - Build code with methods
+        {
+
+        }
+    }
+}
 
 
 
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271b676a4944ee149a39504361ae63c166968a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a762c5869c0d12905b906658c5b9ae120f4082")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
