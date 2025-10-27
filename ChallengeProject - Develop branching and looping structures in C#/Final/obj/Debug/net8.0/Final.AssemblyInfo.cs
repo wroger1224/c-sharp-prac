607ac715219a92ca2d671f0d9d5cbd8849eb5a45
wroger1224 <wroger1224@gmail.com>
2025-10-27 10:47:07 -0400

    Starting new module

--- ChallengeProject - Develop branching and looping structures in C#/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject - Develop branching and looping structures in C#/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271b676a4944ee149a39504361ae63c166968a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661c8a1e587081eb831fcfa3acc69f2360078e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
