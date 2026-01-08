30c6ad759831f4ceaad1617f8b42e4cb2ad523ca
wroger1224 <wroger1224@gmail.com>
2026-01-08 15:50:23 -0500

    little stuff

--- ChallengeProject - Develop branching and looping structures in C#/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject - Develop branching and looping structures in C#/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fbe048d85df0f1afc67b7cfe7e0680d122c77a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08ba9264249bf14bb0fc03a0459d00e7cfee213")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1089,6 +1089,25 @@ using System.Windows.Markup;
                 return true;
             } */
         }
+        //Exercise - Return arrays from methods
+        {
+            int target = 60;
+            int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+            int[] result = TwoCoins(coins, target);
+            
+            int [] TwoCoins(int[] coins, int target)
+            {
+                for (int curr = 0; curr < coins.Length; curr++)
+                {
+                    for (int next = curr +1; next < coins.Length; next++)
+                    {
+                        if (coins[curr] + coins[next] == target) return new int[]{curr, next};
+                    }
+                }
+
+                return new int[0];
+            }
+        }
     }
 }
 
